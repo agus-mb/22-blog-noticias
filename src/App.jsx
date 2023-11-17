5251ebe83d2621e1e1070756b3896f7b0fb5a6d7
agus-mb <munosagustina3@gmail.com>
2023-11-16 22:05:04 -0300

    creacion de main, colores en objeto, css basico

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { Header } from "./mis-componentes/header"
+import { MainArticle } from "./mis-componentes/MainArticle"
 
 function App() {
  
   return (
     <main className="
     px-4
-    pt-6
-    
-
-    ">
-     <Header></Header>
+    pt-6">
+     <Header/>
+     <MainArticle/>
     </main>
   )
 }
