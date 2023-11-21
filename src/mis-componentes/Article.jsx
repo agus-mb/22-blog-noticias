6ab5f344f82e878cc29bb94ab3d846b2ed207bce
agus-mb <munosagustina3@gmail.com>
2023-11-20 22:37:48 -0300

    section lista, css completo

--- src/mis-componentes/Article.jsx
+++ src/mis-componentes/Article.jsx
@@ -1,14 +1,14 @@
-import imgArticle from '../images/image-retro-pcs.jpg'
-export const Article = ({tittle, subtext, text}) => {
+
+export const Article = ({img, tittle, subtext, text}) => {
   return (
-    <article className='flex'>
-        <div className='w-[100px'>
-<img src={imgArticle} alt="" />
+    <article className='flex h-[162px] '>
+        <div className='w-[100px] flex-none'>
+<img src={img} alt="" />
         </div>
         <div className='pl-6'>
             <p className='text-GrayishBlue text-3xl mb-[18px] font-bold'>{tittle}</p>
-            <h2 className='font-bold mb-[18px] hover:text-SoftOrange'>{subtext}</h2>
-            <p>{text}</p>
+            <h2 className='font-bold mb-[18px] hover:text-SoftOrange cursor-pointer'>{subtext}</h2>
+            <p className='text-GrayishBlue text-[16px]'>{text}</p>
 
         </div>
     </article>
--- src/mis-componentes/ArticlesContainer.jsx
+++ src/mis-componentes/ArticlesContainer.jsx
@@ -1,17 +1,24 @@
 import { Article } from "./Article"
+import img1 from '../images/image-retro-pcs.jpg'
+import img2 from '../images/image-top-laptops.jpg'
+import img3 from '../images/image-gaming-growth.jpg'
+
+
 export const ArticlesContainer = () => {
   return (
-    <section>
+    <section className="mt-6">
     <Article
-
+img={img1}
      tittle="01"
       subtext="Reviving Retro PCs"
      text="  What happens when old PCs are given modern upgrades?"/>
     <Article 
+    img={img2}
      tittle="02"
      subtext="Top 10 Laptops of 2022"
      text="Our best picks for various needs and budgets."/>
     <Article
+    img={img3}
      tittle="03"
      subtext="The Growth of Gaming"
      text="How the pandemic has sparked fresh opportunities."/>
